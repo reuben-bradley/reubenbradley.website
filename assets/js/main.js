f348a570458cc9629d62d76d49e965d543dc374a
Reuben Bradley <reuben.m.bradley@gmail.com>
2018-12-23 11:53:41 +0000

    Add Global Game Jam games to development section
    Fix layout styling and flip animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 if ('addEventListener' in window) {
     window.addEventListener('load', function() {
-        console.log('window loaded!');
+        var main = document.getElementById('main');
+        var games = document.getElementById('games');
         var link = document.getElementById('games-link');
         var returnLink = document.getElementById('games-return');
         var wrapper = document.getElementById('wrapper');
@@ -8,10 +9,18 @@ if ('addEventListener' in window) {
         link.addEventListener('click', function(e) {
             e.preventDefault();
             wrapper.classList.add('flipped');
+            setTimeout(function() {
+                main.style.display = 'none';
+                games.style.display = 'block';
+            }, 250);
         });
         returnLink.addEventListener('click', function(e) {
             e.preventDefault();
             wrapper.classList.remove('flipped');
+            setTimeout(function() {
+                games.style.display = 'none';
+                main.style.display = 'block';
+            }, 250)
         });
     });
 }
