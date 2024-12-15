384a77cced53ce132e28a11e2b0dab2005f9d423
Reuben Bradley <reuben.m.bradley@gmail.com>
2024-12-14 21:38:33 -0500

    Finalize webpack config and fix sass errors for build

---
+++ src/assets/js/index.js
@@ -0,0 +1,4 @@
+import '../sass/main.scss';
+
+import './main.js';
+console.log('Hello World!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,84 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/assets/js/main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '',
-        filename: 'js/bundle.js'
+  mode: 'development',
+  entry: {
+    main: './src/assets/js/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    filename: 'js/[name].bundle.js'
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[base][query]',
+        }
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'webfonts/[base][query]',
+        }
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
-    mode: 'development',
-    
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                    {
-                        loader: "postcss-loader"
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("sass")
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: 'images'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|ttf|otf|eot)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: 'fonts'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "bundle.css"
-        })
-    ]
+    port: 9000,
+  },
 };
